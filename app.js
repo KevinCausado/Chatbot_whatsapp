3295b7ad718d5a532399051093ae5a1d9d11e74d
KevinCausado <kevincausado@gmail.com>
2024-04-18 13:20:45 -0500

    HW-6.Fixing Menu

--- app.js
+++ app.js
@@ -11,83 +11,61 @@ let flowContacto = null;
 
 // Flujo para volver al menú anterior
 const flowVolverMenuPrincipal = addKeyword(['Volver', 'volver', 'regresar']).addAnswer('🙌 Hola, bienvenido a nuestro servicio de streaming bot!')
-.addAnswer([
-  '¿Qué te gustaría hacer?',
-  '*Netflix* para Ver Netflix',
-  '*HBO* para Ver HBO',
-  '*98* Contactar al proveedor en caso de emergencia',
-], null, async (context, { flowDynamic }) => {
-  const option = context.body.trim();
-  if (option === 'Netflix') {
-    return await flowDynamic(flowNetflix);
-  } else if (option === 'HBO') {
-    return await flowDynamic(flowHBO);
-  } else if (option === '98') {
-    return await flowDynamic(flowContacto);
-  }
-});
-
-// Flujo para volver al menú Netflix
-const flowVolverMenuNetflix = addKeyword(['atras', 'Atras']).addAnswer([
-  '🎬 Descubre las últimas películas y series en Netflix',
-  '*Precio* para ver precio',
-  '*Pago* para ver métodos de pago',
-  '*Volver* para volver al menú principal'
-
-], null, async (context, { flowDynamic }) => {
-  const option = context.body.trim();
-  if (option === 'Precio') {
-    return await flowDynamic(flowPrecioNetflix);
-  } else if (option === 'Pago') {
-    return await flowDynamic(flowMetodosPagoNetflix);
-  }else if (option === 'Volver') {
-    return await flowDynamic(flowVolver);
-  }
-});
-
-const flowVolverMenuHBO = addKeyword(['atras', 'Atras']).addAnswer([
-  '🎬 Descubre las últimas películas y series en Netflix',
-  '*Precio* para ver precio',
-  '*Pago* para ver métodos de pago',
-  '*Volver* para volver al menú principal'
-
-], null, async (context, { flowDynamic }) => {
-  const option = context.body.trim();
-  if (option === 'Precio') {
-    return await flowDynamic(flowPrecioHBO);
-  } else if (option === 'Pago') {
-    return await flowDynamic(flowMetodosPagoNetflix);
-  }else if (option === 'Volver') {
-    return await flowDynamic(flowVolver);
-  }
-});
-
+.addAnswer('🙌 Hola, bienvenido a nuestro servicio de streaming bot!')
+  .addAnswer([
+    '¿Qué te gustaría hacer?',
+    '*1* para Ver Netflix',
+    '*2* para Ver HBO',
+    '*Amazon Prime Video* para Ver Amazon Prime Video',
+    '*Paramount* para Ver Paramount+',
+    '*Deezer* para Ver Deezer',
+    '*Spotify* para Ver Spotify',
+    '*Youtube Music* para Ver Youtube Music',
+    '*Crunchyroll* para Ver Crunchyroll',
+    '*Disney* para Ver Disney+',
+    '*Star+* para Ver Star+',
+    '*Win+* para Ver Win+',
+    '*IPTV* para Ver IPTV',
+    '*Pago* para ver Metodos de Pago',
+    '*98* Contactar al proveedor en caso de emergencia',
+  ], null, async (context, { flowDynamic }) => {
+    const option = context.body.trim();
+    if (option === '1') {
+      return await flowDynamic(flowNetflix);
+    }else if (option === '2') {
+      return await flowDynamic(flowHBO);
+    }else if (option === 'Pago') {
+      return await flowDynamic(flowMetodosPago);
+    }else if (option === '98') {
+      return await flowDynamic(flowContacto);
+    }
+  });
 
 // Flujo secundario para la opción 1 (Ver precio de Netflix)
-const flowPrecioNetflix = addKeyword(['Precio', 'precio'])
+const flowPrecioNetflix = addKeyword(['3'])
 .addAnswer([
   '💰 El precio de Netflix es $9.99 por mes.',
-  '*Atras* para volver al menú anterior',  
+  '*Volver* para volver al menú anterior',  
 ], null, async (context, { flowDynamic }) => {
   const option = context.body.trim();
-  if (option === 'Atras') {
-    return await flowDynamic(flowVolverMenuNetflix);
+  if (option === 'Volver') {
+    return await flowDynamic(flowVolverMenuPrincipal);
   }
 });
 
-const flowPrecioHBO = addKeyword(['Precio', 'precio'])
+const flowPrecioHBO = addKeyword(['4'])
 .addAnswer([
   '💰 El precio de HBO es $10 por mes.',
-  '*Atras* para volver al menú anterior',  
+  '*Volver* para volver al menú principal',  
 ], null, async (context, { flowDynamic }) => {
   const option = context.body.trim();
-  if (option === 'Atras') {
-    return await flowDynamic(flowVolverMenuHBO);
+  if (option === 'Volver') {
+    return await flowDynamic(flowVolverMenuPrincipal);
   }
 });
 
 // Flujo secundario para la opción 2 (Ver métodos de pago)
-const flowMetodosPagoNetflix = addKeyword(['Pago', 'pago']).addAnswer([
+const flowMetodosPago = addKeyword(['Pago', 'pago']).addAnswer([
   '💳 Aquí están los métodos de pago',
     '',
     '- Nequi:3152089391',
@@ -95,65 +73,43 @@ const flowMetodosPagoNetflix = addKeyword(['Pago', 'pago']).addAnswer([
     '- Davivienda:3152089391',
     '- Daviplata:3152089391',
     '',    
-    '*Atras* para volver al menú anterior'
+    '*Atras* para volver al menú principal'
 
 ], null, async (context, { flowDynamic }) => {
   const option = context.body.trim();
   if (option === 'Atras') {
-    return await flowDynamic(flowVolverMenuNetflix);
+    return await flowDynamic(flowVolverMenuPrincipal);
   }
 });
 
-const flowMetodosPagoHBO = addKeyword(['Pago', 'pago']).addAnswer([
-  '💳 Aquí están los métodos de pago para HBO',
-    '',
-    '- Nequi:3152089391',
-    '- Bancolombia:3152089391',
-    '- Davivienda:3152089391',
-    '- Daviplata:3152089391',
-    '',    
-    '*Atras* para volver al menú anterior'
-
-], null, async (context, { flowDynamic }) => {
-  const option = context.body.trim();
-  if (option === 'Atras') {
-    return await flowDynamic(flowVolverMenuHBO);
-  }
-});
 
   // Flujo principal para Netflix
-flowNetflix = addKeyword(['Netflix', 'netflix'])
+flowNetflix = addKeyword(['1'])
   .addAnswer([
     '🎬 Descubre las últimas películas y series en Netflix',
-    '*Precio* para ver precio',
-    '*Pago* para ver métodos de pago',
+    '*3* para ver precio',    
     '*Volver* para volver al menú principal'
 
   ], null, async (context, { flowDynamic }) => {
     const option = context.body.trim();
-    if (option === 'Precio') {
+    if (option === '3') {
       return await flowDynamic(flowPrecioNetflix);
-    } else if (option === 'Pago') {
-      return await flowDynamic(flowMetodosPagoNetflix);
-    }else if (option === 'Volver') {
+    } else if (option === 'Volver') {
       return await flowDynamic(flowVolverMenuPrincipal);
     }
   });
 
-  flowHBO = addKeyword(['HBO', 'hbo'])
+  flowHBO = addKeyword(['2'])
   .addAnswer([
     '🎬 Descubre las últimas películas y series en HBO',
-    '*Precio* para ver precio',
-    '*Pago* para ver métodos de pago',
+    '*4* para ver precio',    
     '*Volver* para volver al menú principal'
 
   ], null, async (context, { flowDynamic }) => {
     const option = context.body.trim();
-    if (option === 'Precio') {
+    if (option === '4') {
       return await flowDynamic(flowPrecioHBO);
-    } else if (option === 'Pago') {
-      return await flowDynamic(flowMetodosPagoNetflix);
-    }else if (option === 'Volver') {
+    } else if (option === 'Volver') {
       return await flowDynamic(flowVolverMenuPrincipal);
     }
   });
@@ -172,39 +128,97 @@ flowContacto = addKeyword(['98', 'contacto', 'emergencia']).addAnswer([
 });
 
 
+// Flujo inicial
+const flowInicio = addKeyword(['Hola', 'hola', 'Ole', 'ole', 'Alo', 'alo', 'Hola!', 'HOLA', 'Hola', 'Hola!', '¡Hola', 'hola!', 'holaaa', 'hii', 'hi', 'Hello', 'HELLO', 'hey', 'Hey', 'buenas', 'Buenas', 'Buenos días', 'Buenas tardes', 'Buenas noches'])
+.addAnswer(' ¡Bienvenido a nuestro servicio de streaming!')
+.addAnswer('Para acceder al menú principal, escribe "Menú" o presiona el botón correspondiente.');
+
+
+
+
+// Flujo principal
+flowPrincipal = addKeyword(['Menú', 'menu', 'principal']) // Agregar palabras clave para el menú principal
+.addAnswer(' ¡Bienvenido al menú principal!')
+.addAnswer([
+  '¿Qué te gustaría hacer?',
+  '*1* para Ver Netflix',
+  '*2* para Ver HBO',
+  '*Amazon Prime Video* para Ver Amazon Prime Video',
+  '*Paramount* para Ver Paramount+',
+  '*Deezer* para Ver Deezer',
+  '*Spotify* para Ver Spotify',
+  '*Youtube Music* para Ver Youtube Music',
+  '*Crunchyroll* para Ver Crunchyroll',
+  '*Disney* para Ver Disney+',
+  '*Star+* para Ver Star+',
+  '*Win+* para Ver Win+',
+  '*IPTV* para Ver IPTV',
+  '*Pago* para ver Metodos de Pago',
+  '*98* Contactar al proveedor en caso de emergencia',
+], null, async (context, { flowDynamic }) => {
+  const option = context.body.trim();
+  if (option === '1') {
+    return await flowDynamic(flowNetflix);
+  }else if (option === '2') {
+    return await flowDynamic(flowHBO);
+  }else if (option === 'Pago') {
+    return await flowDynamic(flowMetodosPago);
+  }else if (option === '98') {
+    return await flowDynamic(flowContacto);
+  }
+})
+.addAnswer('¿En qué puedo ayudarte?');
+
+
+
 // Flujo principal para mostrar las opciones principales
-flowPrincipal = addKeyword(['Hola', 'hola', 'Ole', 'ole', 'Alo', 'alo', 'Hola!', 'HOLA', 'Hola', 'Hola!', '¡Hola', 'hola!', 'holaaa', 'hii', 'hi', 'Hello', 'HELLO', 'hey', 'Hey', 'buenas', 'Buenas', 'Buenos días', 'Buenas tardes', 'Buenas noches'])
-  .addAnswer('🙌 Hola, bienvenido a nuestro servicio de streaming bot!')
-  .addAnswer([
-    '¿Qué te gustaría hacer?',
-    '*Netflix* para Ver Netflix',
-    '*HBO* para Ver HBO',
-    '*Amazon Prime Video* para Ver Amazon Prime Video',
-    '*Paramount* para Ver Paramount+',
-    '*Deezer* para Ver Deezer',
-    '*Spotify* para Ver Spotify',
-    '*Youtube Music* para Ver Youtube Music',
-    '*Crunchyroll* para Ver Crunchyroll',
-    '*Disney* para Ver Disney+',
-    '*Star+* para Ver Star+',
-    '*Win+* para Ver Win+',
-    '*IPTV* para Ver IPTV',
-    '*98* Contactar al proveedor en caso de emergencia',
-  ], null, async (context, { flowDynamic }) => {
-    const option = context.body.trim();
-    if (option === 'Netflix') {
-      return await flowDynamic(flowNetflix);
-    }else if (option === 'HBO') {
-      return await flowDynamic(flowHBO);
-    }else if (option === '98') {
-      return await flowDynamic(flowContacto);
-    }
-  });
+// flowPrincipal = addKeyword(['Hola', 'hola', 'Ole', 'ole', 'Alo', 'alo', 'Hola!', 'HOLA', 'Hola', 'Hola!', '¡Hola', 'hola!', 'holaaa', 'hii', 'hi', 'Hello', 'HELLO', 'hey', 'Hey', 'buenas', 'Buenas', 'Buenos días', 'Buenas tardes', 'Buenas noches'])
+//   .addAnswer('🙌 Hola, bienvenido a nuestro servicio de streaming bot!')
+//   .addAnswer([
+//     '¿Qué te gustaría hacer?',
+//     '*1* para Ver Netflix',
+//     '*2* para Ver HBO',
+//     '*Amazon Prime Video* para Ver Amazon Prime Video',
+//     '*Paramount* para Ver Paramount+',
+//     '*Deezer* para Ver Deezer',
+//     '*Spotify* para Ver Spotify',
+//     '*Youtube Music* para Ver Youtube Music',
+//     '*Crunchyroll* para Ver Crunchyroll',
+//     '*Disney* para Ver Disney+',
+//     '*Star+* para Ver Star+',
+//     '*Win+* para Ver Win+',
+//     '*IPTV* para Ver IPTV',
+//     '*Pago* para ver Metodos de Pago',
+//     '*98* Contactar al proveedor en caso de emergencia',
+//   ], null, async (context, { flowDynamic }) => {
+//     const option = context.body.trim();
+//     if (option === '1') {
+//       return await flowDynamic(flowNetflix);
+//     }else if (option === '2') {
+//       return await flowDynamic(flowHBO);
+//     }else if (option === 'Pago') {
+//       return await flowDynamic(flowMetodosPago);
+//     }else if (option === '98') {
+//       return await flowDynamic(flowContacto);
+//     }
+//   });
 
 // Función principal para crear el bot
 const main = async () => {
     const adapterDB = new MockAdapter();
-    const adapterFlow = createFlow([flowPrincipal,flowNetflix, flowPrecioNetflix,flowHBO,flowPrecioHBO, flowMetodosPagoNetflix, flowContacto, flowVolverMenuPrincipal,flowVolverMenuNetflix]);
+    const adapterFlow = createFlow([
+        flowInicio,
+      
+      flowPrincipal,flowNetflix, flowPrecioNetflix,flowHBO,flowPrecioHBO, 
+      
+      
+      flowMetodosPago,
+      
+      
+      flowContacto, flowVolverMenuPrincipal,
+      
+      
+      ]);
     const adapterProvider = createProvider(BaileysProvider);
 
     createBot({
